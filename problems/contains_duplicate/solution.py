4acd1764b4ddad2d0a3eccc348fcd8a269f814ab
Viktoria Jechsmayr <viktoria@jechsmayr.at>
2020-10-01 20:17:55 +0000

    Sync LeetCode submission - Contains Duplicate (python3)

---
+++ problems/contains_duplicate/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        Set=set(nums)
+        if len(Set)==len(nums):
+            return False
+        else:
+            return True
