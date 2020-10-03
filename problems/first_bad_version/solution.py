b936b959d716b12e3f6b0e971bacef382f9e4227
Viktoria Jechsmayr <viktoria@jechsmayr.at>
2020-10-03 19:38:06 +0000

    Sync LeetCode submission - First Bad Version (python3)

---
+++ problems/first_bad_version/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l, r, ans = 1, n, -1
+        while l <= r:
+            mid = (l + r) // 2
+            if isBadVersion(mid):
+                ans = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return ans
