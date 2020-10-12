1b3d306b985640f0ec24858878688a2b991fc861
nagahshi <willian.empari@gmail.com>
2020-10-12 12:15:56 -0300

    feat: 23. Merge k Sorted Lists

---
+++ 023_MergeKSortedLists.js
@@ -0,0 +1,42 @@
+class Node {
+    constructor (element) {
+        this.element = element;
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor () {
+        this.head = null;
+        this.size = 0;
+    }
+
+    add (element) {
+        var node = new Node(element);
+        var current;
+
+        if (this.head == null)
+            this.head = node;
+        else {
+            current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.size ++;
+    }
+}
+
+var ll = new LinkedList();
+
+var mergeKLists = function (lists) {
+    lists
+        .flat()
+        .map(el => (Number.isInteger(el) ? el : null))
+        .filter(el => el !== null)
+        .sort()
+        .forEach(el => ll.add(el))
+
+    return ll
+};
